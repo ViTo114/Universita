a348bb1ed5eb57b1e774c77de278df43ef54c1e4
ViTo114 <marzoccavito01@gmail.com>
2025-07-04 17:11:51 +0200

    vault backup: 2025-07-04 17:11:51

--- Primo anno/Primo semetre/Machine Learning/Esercitazione laboratorio/LinearRegression.py
+++ Primo anno/Primo semetre/Machine Learning/Esercitazione laboratorio/LinearRegression.py
@@ -32,7 +32,7 @@ class LinearRegression:
         #Questa variabile mi definisce il nuemero di istanze di addestarmento
         m = len (X_training)
 
-        for epoca in self.numeroIterate:
+        for epoca in range(0, self.numeroIterate)
 
 
 
